17dbd1477c2257e4790fa24dc1ce8feb5755bd75
Janvi Karn <Janvi Karn@LAPTOP-NT3JDOCB>
2024-08-29 19:25:30 +0530

    Add project files.

---
+++ myFirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c = System.Console;
+
+namespace myFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            c.WriteLine("Hello World!!!");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+              
+            }
+            c.ReadKey();
+        }
+    }
+}
